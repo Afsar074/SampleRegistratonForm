7e8ebe9ca06d261cf6f1712bdf350bb82e173c5e
Afsar074 <66517056+Afsar074@users.noreply.github.com>
2021-04-15 00:31:06 +0530

    Update job_dsl_script_mavenProject.groovy

--- job_dsl_script_mavenProject.groovy
+++ job_dsl_script_mavenProject.groovy
@@ -1,13 +1,13 @@
 job('sample-dsl-script'){
     description("job is to create a job automatically when ever run seed jobs ${new Date()}")
     scm{
-        git("https://github.com/Afsar074/SampleRegistratonForm.git",master)
+        git("https://github.com/Afsar074/SampleRegistratonForm.git", master)
     }
     triggers{
         scm('* * * * *')
     }
     steps{
-        maven('clean package','pom.xml')
+        maven('clean package', 'pom.xml')
     }
     publishers{
         archiveArtifacts '**/*.war'
